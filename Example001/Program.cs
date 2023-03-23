6ab9b8d6221ea12bf18892e43224d359c5806186
Dmitrii <dmkuz91@gmail.com>
2023-03-23 15:15:52 +0700

    выполнили первое задание по нахождению квадрата целого числа

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Демонстрация решения
+// Напишите программу, которая на вход
+// принимает число и выдаёт его квадрат (число
+// умноженное на само себя).
+// Например:
+// 4 -> 16
+// -3 -> 9
+// -7 -> 49
+
+Console.WriteLine("Введите целое число ");
+string str = Console.ReadLine();
+int number = Convert.ToInt32(str);
+int squere = number * number;
+Console.WriteLine($"Квадрат числа {number} = {squere}");
